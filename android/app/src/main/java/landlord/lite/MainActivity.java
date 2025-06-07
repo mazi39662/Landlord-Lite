0a9c1e7b52cf8f64603b6146cc0be759814d54b9
Terence Franz Indenerio <terence.indenerio@bapplware.com>
2025-06-07 21:02:42 +0800

    Updated: Dependencies

---
+++ android/app/src/main/java/landlord/lite/MainActivity.java
@@ -0,0 +1,5 @@
+package landlord.lite;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "landlord.lite",
+  appName: "landlord-lite",
+  webDir: "dist",
+};
+
+export default config;
